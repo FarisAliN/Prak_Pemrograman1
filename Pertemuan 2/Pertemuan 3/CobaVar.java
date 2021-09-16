e552099304199730668b85a7c9c541c630f83706
Faris Ali Naufal <farisali.2021@student.uny.ac.id>
2021-09-16 23:45:29 +0700

    belajar tipe data di python

---
+++ Pertemuan 2/Pertemuan 3/CobaVar.java
@@ -0,0 +1,14 @@
+public class CobaVar {
+    public static void main(String [] args){
+        String namaDepan = "Faris";
+        String namaBelakang = "Ali Naufal";
+        int umur = 18;
+        double tinggiBadan = 170;
+        double beratBadan = 68.0;
+
+        System.out.println("Nama : " + namaDepan + " " + namaBelakang);
+        System.out.println("Umur : " + umur + " tahun");
+        System.out.println("Tinggi Badan : " + (int)tinggiBadan + " cm");
+        System.out.println("Berat Badan : " + beratBadan + " kg");
+    }
+}
---
+++ Pertemuan 2/Pertemuan 3/CobaVar.py
@@ -0,0 +1,10 @@
+namaDepan = "Faris"
+namaBelakang = "Ali Naufal"
+umur = 18
+tinggiBadan = 1.70
+beratBadan = 68.0
+
+print("Nama saya adalah " + namaDepan + namaBelakang)
+print("Umur saya adalah " + str(umur) + " tahun")
+print("Tinggi badan saya adalah " + str(tinggiBadan) + " m")
+print("Tinggi badan saya adalah " + str(beratBadan) + " kg")
