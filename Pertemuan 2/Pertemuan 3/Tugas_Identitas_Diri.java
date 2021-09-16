948b850eefad4c7f9de1e411ab658dd2ffef1148
Faris Ali Naufal <farisali.2021@student.uny.ac.id>
2021-09-17 01:58:16 +0700

    Tugas Pertemuan 3

---
+++ Pertemuan 2/Pertemuan 3/Tugas_Identitas_Diri.java
@@ -0,0 +1,23 @@
+public class Tugas_Identitas_Diri {
+    public static void main(String[] args) {
+        String namaLengkap = "Faris Ali Naufal";
+        String nim = "21537144031";
+        int tahunAngkatan = 2021;
+        String programStudi = "Teknologi Informasi";
+        String jurusan = "JPTEI";
+        String fakultas = "Fakultas Teknik";
+        double ipkHarapan = 3.7;
+        System.out.println("~Identitas Diri~ :");
+        System.out.println();
+        System.out.println("Nama Lengkap  : " + namaLengkap);
+        System.out.println("NIM           : " + nim);
+        System.out.println("Angkatan      : " + tahunAngkatan);
+        System.out.println("Program Studi : " + programStudi);
+        System.out.println("Jurusan       : " + jurusan);
+        System.out.println("Fakultas      : " + fakultas);
+        System.out.println("Nama PTN      : Universitas Negeri Yogyakarta");
+        System.out.println("Target IPK    : " + ipkHarapan);
+        System.out.println();
+        System.out.println("Terimakasih.");
+    }
+}
---
+++ Pertemuan 2/Pertemuan 3/Tugas_Identitas_Diri.py
@@ -0,0 +1,16 @@
+nama = "Faris Ali Naufal"
+nim = 21537144031
+angkatan = 2021
+programStudi = "Teknologi Informasi"
+jurusan = "JPTEI"
+fakultas = "Teknik"
+ipkHarapan = "3.7"
+
+print("Berikut adalah identitas diri saya : ")
+print("Nama Lengkap          : " + nama)
+print("Nomor Induk Mahasiswa : " + str(nim))
+print("Tahun Angkatan        : " + str(angkatan))
+print("Program Studi         : " + programStudi)
+print("Jurusan               : " + jurusan)
+print("Fakultas              : " + fakultas)
+print("Ipk Harapan           : " + ipkHarapan)
